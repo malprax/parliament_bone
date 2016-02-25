6566dc1ed5f88dd6ce270f3457da3a5566b654e3
malprax <kingmalprax@gmail.com>
2016-02-25 16:36:08 +0800

    add modified

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160225025059) do
+ActiveRecord::Schema.define(version: 20160225082242) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,10 @@ ActiveRecord::Schema.define(version: 20160225025059) do
     t.string   "jabatan_badan_legislatif"
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
