687a17e95d06a99cac6fa6de5482a43d14b461f6
malprax <kingmalprax@gmail.com>
2016-02-28 17:27:24 +0800

    modifiy model

--- app/models/parliament.rb
+++ app/models/parliament.rb
@@ -1,7 +1,7 @@
 class Parliament < ActiveRecord::Base
-  if Rails.env.development?
+  # if Rails.env.development?
       has_attached_file :avatar, styles: { medium: "300x300>", thumb: "50x50>" }, default_url: "unknown_person.png"
-  end
+  # end
 
  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
  validates_attachment_file_name :avatar, matches: [/png\Z/, /jpe?g\Z/]
