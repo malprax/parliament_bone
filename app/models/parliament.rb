f6f8df8f622cc2135ecbdb083f1b374a1becd53f
malprax <kingmalprax@gmail.com>
2016-03-16 10:21:48 +0800

    modify parliament.rb

--- app/models/parliament.rb
+++ app/models/parliament.rb
@@ -3,6 +3,6 @@ class Parliament < ActiveRecord::Base
       has_attached_file :avatar, styles: { medium: "300x300>", thumb: "50x50>" }, default_url: "unknown_person.png"
   # end
 
- validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
- validates_attachment_file_name :avatar, matches: [/png\Z/, /jpe?g\Z/]
+ # validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+ # validates_attachment_file_name :avatar, matches: [/png\Z/, /jpe?g\Z/]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -37,10 +37,6 @@ ActiveRecord::Schema.define(version: 20160316014404) do
     t.string   "jabatan_badan_legislatif"
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
-    t.integer  "avatar_file_size"
-    t.datetime "avatar_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
