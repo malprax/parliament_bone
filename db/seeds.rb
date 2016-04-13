51970c8c7f3b53b4f1eaf2aee9055322ad4e0118
malprax <kingmalprax@gmail.com>
2016-04-13 22:56:48 +0800

    modify seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,24 +5,36 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+# t.string :name
+# t.string :tempat_lahir
+# t.date   :tanggal_lahir
+# t.string :dapil
+# t.string :fraksi
+# t.string :jabatan_fraksi
+# t.string :komisi
+# t.string :jabatan_komisi
+# t.string :jabatan_badan_anggaran
+# t.string :jabatan_badan_musyawarah
+# t.string :jabatan_badan_kehormatan
+# t.string :jabatan_badan_legislatif
 
 #parliament_member
-Parliament.create(name: 'Al Maghfirah Tiara Hisbaini')
-Parliament.create(name: 'Abdullah Fath')
-Parliament.create(name: 'Aulia Sabril')
-Parliament.create(name: 'Riskawati Angraini')
-Parliament.create(name: 'Nurul Islami')
-Parliament.create(name: 'Muhammad Khadafi Adnan')
-Parliament.create(name: 'Adhiguna Utama Sabril')
-Parliament.create(name: 'Abdullah Fath')
-Parliament.create(name: 'Resky Amalia Putri')
-
-Parliament.create(name: 'Al Maghfirah Tiara Hisbaini')
-Parliament.create(name: 'Abdullah Fath')
-Parliament.create(name: 'Aulia Sabril')
-Parliament.create(name: 'Riskawati Angraini')
-Parliament.create(name: 'Nurul Islami')
-Parliament.create(name: 'Muhammad Khadafi Adnan')
+Parliament.create(name: 'Drs. Andi Akbar Yahya, MM', tempat_lahir: 'Timurung', tanggal_lahir: '20 April 1970', fraksi: 'Golongan Karya')
+Parliament.create(name: 'Andi Samsidar Ishak', tempat_lahir: 'Arasoe', tanggal_lahir: '5 April 1971', fraksi: 'Gerindra')
+Parliament.create(name: 'Drs. A. Taufiq Kadir, MH', tempat_lahir: 'Libureng', tanggal_lahir: '9 September 1957', fraksi: 'Nasdem')
+Parliament.create(name: 'Drs. H. A. M. Amin Mangussara', tempat_lahir: 'Bone', tanggal_lahir: '25 September 1944', fraksi: 'Golongan Karya', komisi: 'I', jabatan_komisi: 'Ketua')
+Parliament.create(name: 'H. Saipullah Latif, SE', tempat_lahir: 'Bone', tanggal_lahir: '3 April 1967', fraksi: 'PNBK', komisi: 'I', jabatan_komisi: 'Wakil Ketua')
+Parliament.create(name: 'Suharni', tempat_lahir: 'Ujung Lamuru', tanggal_lahir: '16 Oktober 1977', fraksi: 'Nasdem', komisi: 'I', jabatan_komisi: 'Sekretaris')
+Parliament.create(name: 'A. Adriana', tempat_lahir: 'Panyiwi', tanggal_lahir: '7 Juni 1976', fraksi: 'PAN', komisi: 'I', jabatan_komisi: 'Anggota')
+Parliament.create(name: 'H. Ramang', tempat_lahir: 'Taccipi', tanggal_lahir: '2 Desember 1960', fraksi: 'Gerindra', komisi: 'I', jabatan_komisi: 'Anggota')
+Parliament.create(name: 'Samsuddin Alfian Kalla, S.Ip', tempat_lahir: 'Panjalili', tanggal_lahir: '1 Februari 1975', fraksi: 'Keadilan Persatuan', komisi: 'I', jabatan_komisi: 'Anggota')
+Parliament.create(name: 'H. Ramang', tempat_lahir: 'Taccipi', tanggal_lahir: '2 Desember 1960', fraksi: 'Golongan Karya', komisi: 'I', jabatan_komisi: 'Anggota')
+Parliament.create(name: 'Samasuddin Alfian Kalla, S.Ip', tempat_lahir: 'Panjalili', tanggal_lahir: '1 Febuari 1975', fraksi: 'Keadilan Persatuan', komisi: 'I', jabatan_komisi: 'Anggota')
+Parliament.create(name: 'Andi Mappanyukki Takka', tempat_lahir: 'Bone', tanggal_lahir: '15 November 1968', fraksi: 'Golongan Karya', komisi: 'I', jabatan_komisi: 'Anggota')
+Parliament.create(name: 'Drs. A. Atoro', tempat_lahir: 'Sanrego', tanggal_lahir: '23 Maret 1967', fraksi: 'Golongan Karya', komisi: 'I', jabatan_komisi: 'Anggota')
+Parliament.create(name: 'Darwis, SH', tempat_lahir: 'Tajong', tanggal_lahir: '1 Juli 1964', fraksi: 'Golongan Karya', komisi: 'I', jabatan_komisi: 'Anggota')
+Parliament.create(name: 'Andi Suaedi, SH, MH', tempat_lahir: 'Sanrego', tanggal_lahir: '31 Desember 1978', fraksi: 'Demokrat', komisi: 'I', jabatan_komisi: 'Anggota')
+Parliament.create(name: 'A. Muh. Idris Alang, SH, MH', tempat_lahir: 'Unra', tanggal_lahir: '12 September 1967', fraksi: 'Golongan Karya', komisi: 'II', jabatan_komisi: 'Ketua')
 Parliament.create(name: 'Adhiguna Utama Sabril')
 Parliament.create(name: 'Abdullah Fath')
 Parliament.create(name: 'Resky Amalia Putri')
