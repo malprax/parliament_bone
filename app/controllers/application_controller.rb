b05ca5c494afe91cc38042f0789026b3b56cb676
malprax <kingmalprax@gmail.com>
2016-05-02 13:50:46 +0700

    still error devise3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,9 @@ class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
   protected
     def configure_permitted_parameters
-       devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
-        devise_parameter_sanitizer.permit(:account_update, keys: [:username])
-         devise_parameter_sanitizer.permit(:sign_in, keys: [:username])
+       devise_parameter_sanitizer.for(:sign_up) << :username
+        devise_parameter_sanitizer.for(:account_update) << :username
+         devise_parameter_sanitizer.for(:sign_in) << :username
       # devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation) }
       # devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation) }
       # devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:username, :email, :password) }
