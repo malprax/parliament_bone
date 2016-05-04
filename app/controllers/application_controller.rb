c2a9851af000d8a5a20f4fa9a81c3f705df76d1a
malprax <kingmalprax@gmail.com>
2016-05-04 09:43:37 +0700

    modify devise again

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,12 @@
 class ApplicationController < ActionController::Base
-  # before_filter :configure_permitted_parameters, if: :devise_controller?
+  before_filter :configure_permitted_parameters, if: :devise_controller?
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  # protect_from_forgery with: :exception
-  # protected
-  #   def configure_permitted_parameters
-  #      devise_parameter_sanitizer.for(:sign_up) << :username
-  #       devise_parameter_sanitizer.for(:account_update) << :username
-  #        devise_parameter_sanitizer.for(:sign_in) << :username
-      # devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation) }
-      # devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation) }
-      # devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:username, :email, :password) }
-
-      # for whitelist all setting
-      # devise_parameter_sanitizer.for(:account_update) {|u| u.permit(:username, :email, :password, :password_confirmation, :current_password)}
-    # end
-
+  protect_from_forgery with: :exception
+  protected
+    def configure_permitted_parameters
+      added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
+      devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+      devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -27,16 +27,32 @@ class User < ActiveRecord::Base
   #virtual attribute_for login after install gem_protected attributes
   #attr_accessible :username, :email, :password, :password_confirmation, :remember_me
 
-  #define login
-  # def login=(login)
-  #  @login = login
-  # end
-  #
-  # def login
-  #  @login || self.username || self.email
-  # end
+  attr_accessor :login
+  validate :validate_username
 
+  define login
+  def login=(login)
+   @login = login
+  end
 
+  def login
+   @login || self.username || self.email
+  end
+
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+     conditions = warden_conditions.dup
+     if login = conditions.delete(:login)
+       where(conditions.to_hash).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+     elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+       where(conditions.to_hash).first
+     end
+   end
   #only allow letter, number, underscore and punctuation
   # validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
 end
