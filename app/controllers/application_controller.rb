987255c41d91500abcb35d6802914941b80e0c1f
malprax <kingmalprax@gmail.com>
2016-05-06 16:30:00 +0700

    add javascript for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   protected
     def configure_permitted_parameters
-      added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
-      devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
-      devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+      #added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
+      devise_parameter_sanitizer.for(:sign_in) << [:username, :email]
+      devise_parameter_sanitizer.for(:sign_up) << [:username, :email]
+      devise_parameter_sanitizer.for(:account_update) << [:username, :email]
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
+  devise_for :users  #, :path => '', :path_names => {:sign_in => 'sign_in', :sign_out => 'sign_out'}
   resources :parliaments do
     collection { post :upload}
   end
