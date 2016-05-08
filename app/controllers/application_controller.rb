1eba628ac46d1b94241274bbee0a32d13b3c7ac3
malprax <kingmalprax@gmail.com>
2016-05-08 12:55:04 +0700

    add modify application.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  after_filter :store_location
   before_filter :configure_permitted_parameters, if: :devise_controller?
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
@@ -10,4 +11,23 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.for(:sign_up) << [:username, :email]
       devise_parameter_sanitizer.for(:account_update) << [:username, :email]
     end
+
+    def store_location
+      # store last url - this is needed for post-login redirect to whatever the user last visited.
+      return unless request.get?
+      if (request.path != "/users/sign_in" &&
+          request.path != "/users/sign_up" &&
+          request.path != "/users/password/new" &&
+          request.path != "/users/password/edit" &&
+          request.path != "/users/confirmation" &&
+          request.path != "/users/sign_out" &&
+          !request.xhr?) # don't store ajax calls
+        session[:previous_url] = request.fullpath
+      end
+    end
+
+    def after_sign_in_path_for(resource)
+      session[:previous_url] || root_path
+    end
+
 end
